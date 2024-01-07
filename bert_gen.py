f30702ec17d4b1f1f22e1ad4e1e577e4ccd7d47d
zcxey2911 <zcxey2911@gmail.com>
2024-01-07 13:59:25 +0800

    edit

--- bert_gen.py
+++ bert_gen.py
@@ -64,10 +64,10 @@ if __name__ == "__main__":
     config_path = args.config
     hps = utils.get_hparams_from_file(config_path)
     lines = []
-    with open(hps.data.training_files, encoding="utf-8") as f:
+    with open(hps.data.training_files.replace("./data","./Data").replace("\\","/"), encoding="utf-8") as f:
         lines.extend(f.readlines())
 
-    with open(hps.data.validation_files, encoding="utf-8") as f:
+    with open(hps.data.validation_files.replace("./data","./Data").replace("\\","/"), encoding="utf-8") as f:
         lines.extend(f.readlines())
     add_blank = [hps.data.add_blank] * len(lines)
 
